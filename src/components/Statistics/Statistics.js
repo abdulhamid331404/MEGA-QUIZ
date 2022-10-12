302dc93d1ee0c29073270f0313dca36a7f24fcae
abdulhamid331404 <freelancerhamid1@gmail.com>
2022-10-12 22:59:51 +0600

    compleate full site respons

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -7,9 +7,9 @@ const Statistics = () => {
         {id:1,name:"React",logo:"https://live.staticflickr.com/65535/52413593240_e00326e727_o.png",total:8},{"id":2,name:"JavaScript","logo":"https://live.staticflickr.com/65535/52412638962_12e932256a_o.png",total:9},{"id":4,name:"CSS","logo":"https://live.staticflickr.com/65535/52413665713_5977a693cb_o.png",total:8},{"id":5,name:"Git","logo":"https://live.staticflickr.com/65535/52412639027_5692c15b3f_o.png",total:11}
     ]
     return (
-        <div className='responsiv-container'>
+        <div className='responsive-container'>
             <h1>Total Quiz Chart</h1>
-            <ResponsiveContainer  width="90%" height={300} >
+            <ResponsiveContainer  width="100%" height={300} >
                 
             <LineChart className='chart-container' data={data}>
             <Line type="monotone" dataKey="total" stroke="#8884d8" strokeWidth={2} />
